3cad05761077cff9a5e871a833fdf4e9dcaa40b7
Educrepaldi <97613407+Educrepaldi@users.noreply.github.com>
2022-01-12 16:27:41 +0100

    Update Main.java
    edu actualización

--- GIT/src/Main.java
+++ GIT/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Holi");
+        System.out.println("Edu ha estado aquí")
     }
 }
